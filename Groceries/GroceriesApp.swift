470882786c6ad41c4405c20b51715c0505106627
shra4747 <shra4747@gmail.com>
2022-07-04 18:00:07 -0400

    Initial Commit

---
+++ Groceries/GroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesApp.swift
+//  Groceries
+//
+//  Created by Shravan Prasanth on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
